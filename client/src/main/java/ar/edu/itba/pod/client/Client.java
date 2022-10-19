a801a4a53f238517b727ecdf7ce7be3d57a54552
Julian Arce <juarce@itba.edu.ar>
2022-10-19 20:47:16 -0300

    feat: basic client and server

--- client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,12 +1,49 @@
 package ar.edu.itba.pod.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
         logger.info("tpe2-g3 Client Starting ...");
+
+        // Client Config
+        final ClientConfig clientConfig = new ClientConfig();
+
+        // Group Config
+        final GroupConfig groupConfig = new GroupConfig()
+                .setName("g3")
+                .setPassword("g3-pass");
+        clientConfig.setGroupConfig(groupConfig);
+
+        // Client Network Config
+        final ClientNetworkConfig clientNetworkConfig = new ClientNetworkConfig();
+        String[] addresses = {"192.168.1.51:5701"};
+        clientNetworkConfig.addAddress(addresses);
+        clientConfig.setNetworkConfig(clientNetworkConfig);
+
+        final HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
+
+        String mapName = "testMap";
+
+        IMap<Integer, String> testMapFromMember = hazelcastInstance.getMap(mapName);
+        testMapFromMember.set(1, "test1");
+
+        IMap<Integer, String> testMap = hazelcastInstance.getMap(mapName);
+        System.out.println(testMap.get(1));
+
+        // Shutdown
+        HazelcastClient.shutdownAll();
     }
 }
--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -1,12 +1,45 @@
 package ar.edu.itba.pod.server;
 
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
+
 public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         logger.info("tpe2-g3 Server Starting ...");
+
+        // Config
+        final Config config = new Config();
+
+        // Group Config
+        final GroupConfig groupConfig = new GroupConfig()
+                .setName("g3")
+                .setPassword("g3-pass");
+        config.setGroupConfig(groupConfig);
+
+        // Network Config
+        final MulticastConfig multicastConfig = new MulticastConfig();
+
+        final JoinConfig joinConfig = new JoinConfig()
+                .setMulticastConfig(multicastConfig);
+
+        final InterfacesConfig interfacesConfig = new InterfacesConfig()
+                .setInterfaces(List.of("192.168.1.*"))
+                .setEnabled(true);
+
+        final NetworkConfig networkConfig = new NetworkConfig()
+                .setInterfaces(interfacesConfig)
+                .setJoin(joinConfig);
+
+        config.setNetworkConfig(networkConfig);
+
+        // Start Cluster
+        Hazelcast.newHazelcastInstance(config);
     }
 }
