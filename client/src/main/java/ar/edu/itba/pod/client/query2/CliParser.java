fbb47741f517828af7d7010abe01689d31b8e46c
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-20 16:43:18 -0300

    [Chore] Added query2 boilerplate implementation

---
+++ client/src/main/java/ar/edu/itba/pod/client/query2/CliParser.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.pod.client.query2;
+
+import ar.edu.itba.pod.client.models.Arguments;
+import ar.edu.itba.pod.client.utils.BaseParser;
+
+public class CliParser extends BaseParser {
+    @Override
+    protected Arguments createArgumentObject() {
+        return new Arguments();
+    }
+}
---
+++ client/src/main/java/ar/edu/itba/pod/client/query2/Client.java
@@ -0,0 +1,30 @@
+package ar.edu.itba.pod.client.query2;
+
+import ar.edu.itba.pod.client.utils.Hazelcast;
+import com.hazelcast.client.HazelcastClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+
+    private final static Logger logger = LoggerFactory.getLogger(Client.class);
+    public static void main(String[] args) {
+        logger.info("Query 2 client starting...");
+        
+        var parser = new CliParser();
+        var arguments = parser.parse(args);
+        
+        if (arguments.isEmpty()) {
+            logger.error("Invalid arguments");
+            return;
+        }
+        
+        try {
+            var hazelcast = Hazelcast.connect(arguments.get());
+        }
+        finally {
+            // Shutdown
+            HazelcastClient.shutdownAll();
+        }
+    }
+}
