0a010c46416a948bacd442fef3ea8a6dc412c72d
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-20 16:43:42 -0300

    [Chore] Added query3 boilerplate implementation and tests

---
+++ client/src/main/java/ar/edu/itba/pod/client/query3/CliParser.java
@@ -0,0 +1,45 @@
+package ar.edu.itba.pod.client.query3;
+
+import ar.edu.itba.pod.client.utils.BaseParser;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+public class CliParser extends BaseParser {
+    @Override
+    protected Arguments createArgumentObject() {
+        return new Arguments();
+    }
+    
+    
+    @Override
+    protected void RegisterOptions(Options options){
+        super.RegisterOptions(options);
+        options.addRequiredOption("Dmin","Dmin",true,"minimum number of people");
+    }
+    
+    @Override
+    protected ar.edu.itba.pod.client.models.Arguments getArguments(CommandLine cmd){
+        Arguments args = (Arguments) super.getArguments(cmd);
+        
+        try {
+            args.setMin(Integer.parseInt(cmd.getOptionValue("Dmin")));
+        }
+        catch (NumberFormatException e) {
+            logger.error("Invalid argument for Dmin: {}", cmd.getOptionValue("Dmin"));
+        }
+        return args;
+    } 
+    
+
+    public static class Arguments extends ar.edu.itba.pod.client.models.Arguments{
+        @Getter @Setter
+        private int min = 0;
+        
+        @Override
+        public boolean isValid() {
+            return super.isValid() && min > 0;
+        }
+    }
+}
---
+++ client/src/test/java/ar/edu/itba/pod/client/query3/CliParserTests.java
@@ -0,0 +1,114 @@
+package ar.edu.itba.pod.client.query3;
+
+import ar.edu.itba.pod.client.Utils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class CliParserTests {
+    private CliParser parser;
+    
+    @BeforeEach
+    public void setUp() {
+        parser = new CliParser();
+    }
+    
+    @Test
+    public void withRequiredParameters_ShouldSucceed(){
+        // Arrange
+        var min = "10000";
+        var args = new String[]{
+                "-Daddresses=192.168.0.5:5701",
+                "-DinPath=" + Utils.getFolderPath("inPath/"),
+                "-DoutPath=" + Utils.getFolderPath("outPath/"),
+                "-Dmin=" + min
+        };
+
+        // Act
+        var arguments = parser.parse(args);
+
+        // Assert
+        arguments.ifPresentOrElse(
+                (parsedArguments) -> {
+                    var arg = (CliParser.Arguments) parsedArguments;
+                    assertThat(arg.getMin()).isEqualTo(Integer.parseInt(min));
+                },
+                () -> fail("Arguments should be parsed")
+        );
+    }
+    
+    @Test
+    public void withoutRequiredParameters_ShouldFail(){
+        // Arrange
+        var args = new String[]{
+                "-Daddresses=192.168.0.5:5701",
+                "-DinPath=" + Utils.getFolderPath("inPath/"),
+                "-DoutPath=" + Utils.getFolderPath("outPath/")
+        };
+
+        // Act
+        var arguments = parser.parse(args);
+
+        // Assert
+        assertThat(arguments).isEmpty();
+    }
+    
+    @Test
+    public void whenMinIsText_ShouldFail(){
+        // Arrange
+        var min = "prueba";
+        var args = new String[]{
+                "-Daddresses=192.168.0.5:5701",
+                "-DinPath=" + Utils.getFolderPath("inPath/"),
+                "-DoutPath=" + Utils.getFolderPath("outPath/"),
+                "-Dmin=" + min
+        };
+
+        // Act
+        var arguments = parser.parse(args);
+
+        // Assert
+        assertThat(arguments).isEmpty();
+    }
+    
+    @Test
+    public void whenMinIsNegative_ShouldFail(){
+        // Arrange
+        var min = "-10000";
+        var args = new String[]{
+                "-Daddresses=192.168.0.5:5701",
+                "-DinPath=" + Utils.getFolderPath("inPath/"),
+                "-DoutPath=" + Utils.getFolderPath("outPath/"),
+                "-Dmin=" + min
+        };
+
+        // Act
+        var arguments = parser.parse(args);
+
+        // Assert
+        assertThat(arguments).isEmpty();
+    }
+    
+    @Test
+    public void whenMinIsZero_ShouldFail(){
+
+        // Arrange
+        var min = "0";
+        var args = new String[]{
+                "-Daddresses=192.168.0.5:5701",
+                "-DinPath=" + Utils.getFolderPath("inPath/"),
+                "-DoutPath=" + Utils.getFolderPath("outPath/"),
+                "-Dmin=" + min
+        };
+
+        // Act
+        var arguments = parser.parse(args);
+
+        // Assert
+        assertThat(arguments).isEmpty();
+    }
+    
+}
